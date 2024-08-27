7509a064bfb645652ef3707a9f4baf3bde3d3d71
ravina-tech <ravina26ghidode@gmail.com>
2024-08-27 18:08:14 +0530

    Jest Configuration

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -3,7 +3,22 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Us Page</h1>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="name"
+          className="border border-black p-2 m-2"
+        />
+        <input
+          type="text"
+          placeholder="message"
+          className="border border-black p-2 m-2"
+        />
+        <button className="border border-black p-2 m-2 rounded-lg bg-gray-100">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
